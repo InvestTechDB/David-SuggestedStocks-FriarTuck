e133b6a2f11a96ad18e80cbb0b0a702c99157b86
Admin <admin@Admins-MacBook-Pro.local>
2018-07-19 14:05:21 -0700

    refactored some path variables, serving on a port now as well.

---
+++ client/src/index.jsx
@@ -0,0 +1,21 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div> 
+                Ayooo!
+            </div>
+        )
+    }
+}
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
